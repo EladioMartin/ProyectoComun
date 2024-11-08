5838df61c49b80d1023424cefa8cc11b63d345f2
EladioMartin <eladio.marper@educa.jcyl.es>
2024-11-08 13:46:06 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+package ProyectoComun;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Ventana12 v = new Ventana12();
+		
+	}
+
+}
---
+++ Ventana12.java
@@ -0,0 +1,49 @@
+package ProyectoComun;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Ventana12 extends JFrame{
+
+	
+	void Ventana12(){
+		
+		this.setResizable(false);
+		this.setSize(393, 852);
+		this.setLayout(new FlowLayout());
+		this.setVisible(true);
+		
+		//PANEL PRINCIPAL
+		JPanel mainPanel= new JPanel();
+		mainPanel.setPreferredSize(new Dimension(393, 852));
+		mainPanel.setBackground(Color.white);
+		
+		this.add(mainPanel);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+	
+	
+}
